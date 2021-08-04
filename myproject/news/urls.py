7007ec14625bd2cbd0470f2aaa61b73cff5bcc56
elahesems <elahesems@gmail.com>
2021-08-04 12:23:06 +0300

    news_detail sayfası eklendi

--- myproject/news/urls.py
+++ myproject/news/urls.py
@@ -4,6 +4,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('news/<str:pk>/', views.new_detail, name='news_detail'),
 
 
 ]
--- myproject/news/views.py
+++ myproject/news/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render, get_object_or_404 ,redirect
-from .models import *
+from .models import News
 # Create your views here.
+
+def new_detail(request,pk):
+    news = News.objects.filter(pk=pk)
+    context = {'news':news}
+    return render(request, 'front/news_detail.html', context)
